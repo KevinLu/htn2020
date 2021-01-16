5f559cd4ad751ec5e7161a33fe7217473f0a9c4e
Franco <francochen2000@hotmail.com>
2021-01-16 00:22:58 -0500

    Add cockroach db and User model

--- server/index.js
+++ server/index.js
@@ -6,13 +6,15 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const passport = require('passport')
 
-const threadRouter = require('./routes/thread.js')
-const usersRouter = require('./routes/users.js')
+const threadRouter = require("./routes/thread.js");
+const usersRouter = require("./routes/users.js");
 
-app.use(cors())
+// import models
+const UserModel = require("./models/User");
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
@@ -28,14 +30,19 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000
 
-// routers
-app.use('/api/users', usersRouter)
-app.use('/api/thread', threadRouter)
+// Add all database objects from models here
+const User = UserModel(sequelize);
+
+sequelize.sync({ force: true }).then(() => {
+  console.log(`Database & tables created!`);
+});
+
+const port = process.env.PORT || 5000;
 
 // routers
-app.use('/api/users', usersRouter)
-app.use('/api/thread', threadRouter)
+app.use("/api/users", usersRouter);
+app.use("/api/thread", threadRouter);
 
 app.listen(port, () => {
-    console.log(`Server Listening on ${port}`)
-})
+  console.log(`Server Listening on ${port}`);
+});
