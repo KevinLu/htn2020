0f3587a23ff9fb9e002e778e944bcceca69ffcc3
gkysaad <georgekysaad@gmail.com>
2021-01-16 01:27:46 -0500

    Added User model and added cockroach files to .gitignore

--- server/index.js
+++ server/index.js
@@ -5,26 +5,29 @@ const cors = require("cors");
 var Sequelize = require("sequelize-cockroachdb");
 var fs = require("fs");
 
-const bodyParser = require('body-parser')
-const passport = require('passport')
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const threadRouter = require('./routes/thread.js')
-const usersRouter = require('./routes/users.js')
+const threadRouter = require("./routes/thread.js");
+const usersRouter = require("./routes/users.js");
 
-app.use(cors())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+// import models
+const UserModel = require("./models/User");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    // All the javascript and css files will be read and served from this folder
-    app.use(express.static(path.join(__dirname, '../client/build')))
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
-    // index.html for all page routes    html or routing and naviagtion
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
-    });
+  // index.html for all page routes    html or routing and naviagtion
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
 }
 
 // Connect to CockroachDB through Sequelize.
@@ -34,11 +37,18 @@ var sequelize = new Sequelize("database", "maxroach", "", {
   logging: false,
 });
 
+// Add all database objects from models here
+const User = UserModel(sequelize);
+
+sequelize.sync({ force: true }).then(() => {
+  console.log(`Database & tables created!`);
+});
+
 const port = process.env.PORT || 5000;
 
 // routers
-app.use('/api/users', usersRouter)
-app.use('/api/thread', threadRouter)
+app.use("/api/users", usersRouter);
+app.use("/api/thread", threadRouter);
 
 app.listen(port, () => {
   console.log(`Server Listening on ${port}`);
