556e7222dea60204a6cff630db4441f88d999e7d
Michael Le <le.michael88@gmail.com>
2021-01-16 11:21:12 -0500

    combine getURL and getJobId

--- server/services/dropbase.js
+++ server/services/dropbase.js
@@ -1,29 +1,17 @@
 const axios = require('axios');
 
-const getPresignedURL = async (token) => {
+// returns presigned url (upload_url) and jobid (job_id)
+const getUrlAndId = async (token) => {
     const url = 'https://api2.dropbase.io/v1/pipeline/generate_presigned_url';
     const data = {"token": token};
     const response = await axios.post(url, data);
     
     if (response.status == 200) {
-        return response.data.upload_url;
+        return response.data;
     } else {
         return "bad request";
     }
     
 }
 
-const getJobId = async (token) => {
-    console.log('asdsa');
-    const url = 'https://api2.dropbase.io/v1/pipeline/generate_presigned_url';
-    const data = {"token": token};
-    const response = await axios.post(url, data);
-  
-    if (response.status == 200) {
-        return response.data.job_id;
-    } else {
-        return "bad request";
-    }
-}
-
-module.exports = {getPresignedURL, getJobId};
+module.exports = {getUrlAndId};
