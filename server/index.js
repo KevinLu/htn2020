16c4a173849c874ab4dc839df0577f174a68450d
gkysaad <georgekysaad@gmail.com>
2021-01-16 10:20:45 -0500

    Merge code

--- server/index.js
+++ server/index.js
@@ -19,15 +19,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  // All the javascript and css files will be read and served from this folder
-  app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    // All the javascript and css files will be read and served from this folder
+    app.use(express.static(path.join(__dirname, '../client/build')))
 
-  // index.html for all page routes    html or routing and naviagtion
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
+    // index.html for all page routes    html or routing and naviagtion
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    });
 }
 
 // Connect to CockroachDB through Sequelize.
