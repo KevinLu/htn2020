fd91496f31039c10068495878866b31e12c15339
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2021-01-16 10:58:53 -0500

    Use CockroachCloud

--- server/index.js
+++ server/index.js
@@ -30,6 +30,20 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 5000
 
+// Connect to CockroachDB through Sequelize.
+var sequelize = new Sequelize('nice-puma-270.defaultdb', 'kevin', 'K4OJov8KbV3clDKY', {
+  host: 'free-tier.gcp-us-central1.cockroachlabs.cloud',
+  dialect: 'postgres',
+  port: 26257,
+  logging: false,
+  dialectOptions: {
+      ssl: {
+          ca: fs.readFileSync('C:/cc-ca.crt.txt')
+              .toString()
+      }
+  }
+});
+
 // Add all database objects from models here
 const User = UserModel(sequelize);
 
