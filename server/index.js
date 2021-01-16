875d3556e63dc83087ac3acbbbef793ee4a74603
Franco <francochen2000@hotmail.com>
2021-01-16 00:22:58 -0500

    Fix formatting issues and add passportjs as a dependency

--- server/index.js
+++ server/index.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const express = require('express')
+const app = express()
+const path = require('path')
 const cors = require('cors')
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const threadRouter = require('./routes/thread.js')
 const usersRouter = require('./routes/users.js')
 
-app.use(cors());
+app.use(cors())
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    // All the javascript and css files will be read and served from this folder
+    app.use(express.static(path.join(__dirname, '../client/build')))
 
-  // Set static folder   
-  // All the javascript and css files will be read and served from this folder
-  app.use(express.static(path.join(__dirname, "../client/build")));
-
-  // index.html for all page routes    html or routing and naviagtion
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
+    // index.html for all page routes    html or routing and naviagtion
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    });
 }
 
 const port = process.env.PORT || 5000
@@ -33,5 +33,5 @@ app.use('/api/users', usersRouter)
 app.use('/api/thread', threadRouter)
 
 app.listen(port, () => {
-  console.log(`Server Listening on ${port}`)
-});
+    console.log(`Server Listening on ${port}`)
+})
