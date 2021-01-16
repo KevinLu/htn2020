b42823c73356e83afaac48d1dde0213bcd59ee7c
Franco <francochen2000@hotmail.com>
2021-01-16 15:12:16 -0500

    Basic create thread form

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ThreadPage from './pages/ThreadPage';
+import CreateThread from './pages/CreateThread';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/thread/:threadUrl" component={ThreadPage} />
+          <Route exact path="/create/thread" component={CreateThread} />
         </Switch>
       </Router>
     </ChakraProvider>
