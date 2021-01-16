51931166e8413429ab1d4a728a4ca7e3f1b673b9
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2021-01-16 13:05:34 -0500

    Add Hero section and ThreadPage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Container} from "@chakra-ui/react"
 import ThreadList from '../components/ThreadList';
+import Hero from '../components/Hero';
 
 function Home() {
   return (
     <div>
+      <Hero />
       <Container maxWidth="100ch">
         <ThreadList />
       </Container>
---
+++ client/src/pages/ThreadPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ThreadPage(props) {
+  return (
+    <div>
+      Thread page
+    </div>
+  );
+}
+
+export default ThreadPage;
