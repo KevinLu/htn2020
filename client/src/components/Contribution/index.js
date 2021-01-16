e7fb53907fad9cc288857ea9a1bb2bb0b5930113
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2021-01-16 14:45:40 -0500

    Finish contribution item

--- client/src/components/Contribution/index.js
+++ client/src/components/Contribution/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, Flex, Avatar, Text, HStack} from '@chakra-ui/react';
+import {Button, Flex, Avatar, Text, HStack} from '@chakra-ui/react';
 
 function Contribution() {
   const data = {
@@ -13,17 +13,20 @@ function Contribution() {
     <Flex
       borderWidth="1px"
       borderRadius="lg"
-      backgroundColor="gray.50"
       width="100%"
       pt={2} pb={2} pl={4} pr={4}
-      flexDir="column">
-      <Text fontSize="lg" fontWeight="600">{data.description}</Text>
-      <HStack mt={2} alignItems="center">
-        <Avatar size="xs" name={data.username} src={data.avatar} />
-        <Text>{data.username}</Text>
-        <Text color="gray.500">{`contributed ${data.timeSince}`}</Text>
-        <Text color="gray.500">{data.fileSize}</Text>
-      </HStack>
+      alignItems="center"
+      justifyContent="space-between">
+      <Flex flexDir="column">
+        <Text fontSize="lg" fontWeight="600">{data.description}</Text>
+        <HStack mt={2} alignItems="center">
+          <Avatar size="xs" name={data.username} src={data.avatar} />
+          <Text>{data.username}</Text>
+          <Text color="gray.500">{`contributed ${data.timeSince} ago`}</Text>
+          <Text color="gray.500">{`(${data.fileSize})`}</Text>
+        </HStack>
+      </Flex>
+      <Button>View</Button>
     </Flex>
   );
 }
