6af8b16cbe929f1622212a287e52b3adde096e17
gkysaad <georgekysaad@gmail.com>
2021-01-16 22:27:54 -0500

    added file picker code

--- client/src/pages/CreateThread.js
+++ client/src/pages/CreateThread.js
@@ -1,31 +1,44 @@
 import {
-    Box,
-    Button,
-    FormControl,
-    FormErrorMessage, Heading,
-    Input,
-    Textarea
-} from "@chakra-ui/react"
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  Heading,
+  Input,
+  Textarea,
+} from '@chakra-ui/react';
 
-import {Field, Form, Formik} from "formik";
-import React, {useEffect} from "react";
-import * as Yup from "yup";
-import Axios from "axios";
+import { Field, Form, Formik } from 'formik';
+import React, { useEffect, useCallback } from 'react';
+import * as Yup from 'yup';
+import Axios from 'axios';
+import { useDropzone } from 'react-dropzone';
 
 function CreateThread() {
-    useEffect(() => {
-        document.body.style.backgroundColor = "#EDF2F7";
-        return (() => {
-            document.body.style.backgroundColor = "#fff";
-        });
-    }, []);
+  useEffect(() => {
+    document.body.style.backgroundColor = '#EDF2F7';
+    return () => {
+      document.body.style.backgroundColor = '#fff';
+    };
+  }, []);
 
-    return (
-        <Formik
-            onSubmit={(values, actions) => {
-                setTimeout(() => {
-                    // TODO:
-                    /*const dataToSubmit = {
+  let formData = new FormData();
+
+  const onDrop = useCallback(acceptedFiles => {
+    acceptedFiles.map(file => {
+      console.log("file: " + file);
+      formData.append('file', file, file.name);
+    });
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <Formik
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          // TODO:
+          /*const dataToSubmit = {
                         user: user.id,
                         pipelineToken: values.pipelineToken,
                         title: values.title,
@@ -41,79 +54,110 @@ function CreateThread() {
                             console.log(err);
                         });
                     setSubmitting(false);*/
-                }, 500);
-            }}
-            initialValues={{}}
-            validationSchema={Yup.object().shape({
-                pipelineToken: Yup.string().required('Please enter a pipeline token'),
-                title: Yup.string()
-                    .min(10, 'Title must be at least 10 characters')
-                    .required('Please enter a title'),
-                message: Yup.string()
-                    .min(10, 'Message must be at least 10 characters')
-                    .required('Please enter a message'),
-            })}>
-            {(props) => (
-                <Box mt="4%"
-                     width={{base: '95%', sm: '800px'}}
-                     backgroundColor="white"
-                     mr="auto"
-                     ml="auto"
-                     p="2.5em"
-                     borderRadius="8px"
-                     boxShadow="0px 4px 25px rgba(0, 0, 0, 0.13)">
-                    <Heading as="h1" size="xl" textAlign="center">
-                        Create a new thread
-                    </Heading>
+        }, 500);
+      }}
+      initialValues={{}}
+      validationSchema={Yup.object().shape({
+        pipelineToken: Yup.string().required('Please enter a pipeline token'),
+        title: Yup.string()
+          .min(10, 'Title must be at least 10 characters')
+          .required('Please enter a title'),
+        message: Yup.string()
+          .min(10, 'Message must be at least 10 characters')
+          .required('Please enter a message'),
+      })}
+    >
+      {props => (
+        <Box
+          mt="4%"
+          width={{ base: '95%', sm: '800px' }}
+          backgroundColor="white"
+          mr="auto"
+          ml="auto"
+          p="2.5em"
+          borderRadius="8px"
+          boxShadow="0px 4px 25px rgba(0, 0, 0, 0.13)"
+        >
+          <Heading as="h1" size="xl" textAlign="center">
+            Create a new thread
+          </Heading>
 
-                    <Form>
-                        <Field name="pipelineToken">
-                            {({field, form}) => (
-                                <FormControl
-                                    mt={4}
-                                    isInvalid={form.errors.pipelineToken && form.touched.pipelineToken}>
-                                    <Input {...field} id="pipelineToken"
-                                           placeholder="Enter a Dropbase pipeline token"/>
-                                    <FormErrorMessage>{form.errors.pipelineToken}</FormErrorMessage>
-                                </FormControl>
-                            )}
-                        </Field>
+          <Form>
+            <Field name="pipelineToken">
+              {({ field, form }) => (
+                <FormControl
+                  mt={4}
+                  isInvalid={
+                    form.errors.pipelineToken && form.touched.pipelineToken
+                  }
+                >
+                  <Input
+                    {...field}
+                    id="pipelineToken"
+                    placeholder="Enter a Dropbase pipeline token"
+                  />
+                  <FormErrorMessage>
+                    {form.errors.pipelineToken}
+                  </FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
 
-                        <Field name="title">
-                            {({field, form}) => (
-                                <FormControl isInvalid={form.errors.title && form.touched.title}
-                                             mt={4}>
-                                    <Input {...field} id="title" placeholder="Enter a title"/>
-                                    <FormErrorMessage>{form.errors.title}</FormErrorMessage>
-                                </FormControl>
-                            )}
-                        </Field>
+            <Field name="title">
+              {({ field, form }) => (
+                <FormControl
+                  isInvalid={form.errors.title && form.touched.title}
+                  mt={4}
+                >
+                  <Input {...field} id="title" placeholder="Enter a title" />
+                  <FormErrorMessage>{form.errors.title}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
 
-                        <Field name="message">
-                            {({field, form}) => (
-                                <FormControl
-                                    isInvalid={form.errors.message && form.touched.message}
-                                    mt={4}>
-                                    <Textarea {...field} id="message"
-                                              placeholder="Enter a message.."/>
-                                    <FormErrorMessage>{form.errors.message}</FormErrorMessage>
-                                </FormControl>
-                            )}
-                        </Field>
+            <Field name="message">
+              {({ field, form }) => (
+                <FormControl
+                  isInvalid={form.errors.message && form.touched.message}
+                  mt={4}
+                >
+                  <Textarea
+                    {...field}
+                    id="message"
+                    placeholder="Enter a message.."
+                  />
+                  <FormErrorMessage>{form.errors.message}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
 
-                        <Button
-                            mt={4}
-                            colorScheme="teal"
-                            isLoading={props.isSubmitting}
-                            type="submit"
-                        >
-                            Submit
-                        </Button>
-                    </Form>
-                </Box>
-            )}
-        </Formik>
-    )
+            <div
+              className="container"
+              style={{ marginTop: '15px', marginBottom: '15px', borderWidth: "2px", borderStyle: 'dashed', padding: '5px' }}
+            >
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                {isDragActive ? (
+                  <p>Drop the files here ...</p>
+                ) : (
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                )}
+              </div>
+            </div>
+
+            <Button
+              mt={4}
+              colorScheme="teal"
+              isLoading={props.isSubmitting}
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Form>
+        </Box>
+      )}
+    </Formik>
+  );
 }
 
 export default CreateThread;
