87725beffe98dc50aa3a0bdefb3a1e923d75d4bd
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2021-01-17 09:39:09 -0500

    Use state to determine login for right menu

--- client/src/components/Navbar/Sections/RightMenu.js
+++ client/src/components/Navbar/Sections/RightMenu.js
@@ -1,37 +1,51 @@
-import React, {useContext} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Flex, Button, ButtonGroup} from "@chakra-ui/react";
 import {ArrowForwardIcon, AddIcon} from '@chakra-ui/icons';
 import {Link} from 'react-router-dom';
 
 function RightMenu() {
-  return (
-    <Flex alignItems="center">
-      {window.localStorage.getItem("loggedIn") !== "true" ?
-        <ButtonGroup>
-          <Link to="/login"><Button variant="ghost">Sign in</Button></Link>
-          <Link to="/register">
-            <Button
-              colorScheme="purple"
-              bgColor="black"
-              color="white"
-              rightIcon={<ArrowForwardIcon />}>
-              Sign up
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    if (window.localStorage.getItem("loggedIn") === "true") {
+      setLoggedIn(true);
+      setIsLoading(false);
+    }
+  }, [window.localStorage]);
+
+  if (isLoading) {
+    return <></>;
+  } else {
+    return (
+      <Flex alignItems="center">
+        {!loggedIn ?
+          <ButtonGroup>
+            <Link to="/login"><Button variant="ghost">Sign in</Button></Link>
+            <Link to="/register">
+              <Button
+                colorScheme="purple"
+                bgColor="black"
+                color="white"
+                rightIcon={<ArrowForwardIcon />}>
+                Sign up
           </Button>
-          </Link>
-        </ButtonGroup> :
-        <ButtonGroup>
-          <Link to="/create/thread">
-            <Button
-              colorScheme="purple"
-              bgColor="black"
-              color="white"
-              rightIcon={<AddIcon />}>
-              New Thread
+            </Link>
+          </ButtonGroup> :
+          <ButtonGroup>
+            <Link to="/create/thread">
+              <Button
+                colorScheme="purple"
+                bgColor="black"
+                color="white"
+                rightIcon={<AddIcon />}>
+                New Thread
             </Button>
-          </Link>
-        </ButtonGroup>}
-    </Flex>
-  );
+            </Link>
+          </ButtonGroup>}
+      </Flex>
+    );
+  }
 }
 
 export default RightMenu;
